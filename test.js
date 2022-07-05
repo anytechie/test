34ce1ea87da973b262ded7f4de74929d4994266f
anytechie <77220267+anytechie@users.noreply.github.com>
2022-07-05 13:27:26 +0530

    add

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ let content = fs.readFileSync(file, 'utf8');
 
 const stack = [];
 
-const createTable = [];
+const scripts = [];
 
 while(true) {
     let idx = content.indexOf('CREATE TABLE');
@@ -17,7 +17,7 @@ while(true) {
         } else if (char === ')') {
             stack.pop();
             if (stack.length === 0) {
-                createTable.push(content.substring(idx, i + 1));
+                scripts.push(content.substring(idx, i + 1));
                 content = content.substring(i + 1);
                 break;
             }
@@ -28,5 +28,27 @@ while(true) {
     }
 }
 
-console.log(createTable);
 
+const tables = [] // [{name, script}]
+
+for(let script of scripts) {
+    let name = script.slice(script.indexOf('TABLE') + 5, script.indexOf('('));
+    name = name.replace(/\s/g, '');
+    name = name.replace(/"/g, '');
+    name = name.split('.')[1];
+    if (!name) {
+        throw new Error('Invalid script');
+    }
+    const table = {name, script};
+    tables.push(table);
+}
+console.log(tables);
+
+// create a new folder
+if (!fs.existsSync('output')) {
+    fs.mkdirSync('output');
+}
+
+for(let table of tables) {
+    fs.writeFileSync(`output/${table.name}.sql`, table.script);
+}
