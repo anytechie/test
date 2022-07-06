ccf1435fa5de7b8aa0db9b8d1ea4b37c41e0a401
anytechie <77220267+anytechie@users.noreply.github.com>
2022-07-06 17:06:35 +0530

    as

--- test.js
+++ test.js
@@ -2,16 +2,19 @@ const fs = require('fs');
 
 // go through all the sql files in the current directory and inside each folder in the current directory
 // create a new file with the name db.changelog-master.xml, and insert each file path as a liquibase changeSet
+// path will be relative to the current directory
 
-fs.readdirSync(__dirname).forEach(function(file) {
-    if (file.indexOf('.') !== 0) {
-        fs.readdirSync(__dirname + '/' + file).forEach(function(file) {
-            if (file.indexOf('.') !== 0) {
-                fs.appendFileSync(__dirname + '/db.changelog-master.xml', '<changeSet author="' + file + '" id="' + file + '" order="1">\n');
-                fs.appendFileSync(__dirname + '/db.changelog-master.xml', '<sqlFile path="' + __dirname + '/' + file + '/' + file + '.sql" />\n');
-                fs.appendFileSync(__dirname + '/db.changelog-master.xml', '</changeSet>\n');
-            }
+fs.readdirSync('./').forEach(function(dir) {
+    if (fs.lstatSync(dir).isDirectory()) {
+        fs.readdirSync(dir).forEach(function(file) {
+        if (file.endsWith('.sql')) {
+            console.log(file);
+            fs.appendFileSync('db.changelog-master.xml', '<changeSet author="system" id="' + file + '" order="1">\n');
+            // path will be relative to the current directory
+            fs.appendFileSync('db.changelog-master.xml', '<sqlFile path="' + dir + '/' + file + '"/>\n');
+            fs.appendFileSync('db.changelog-master.xml', '</changeSet>\n');
+        }
         });
     }
-}
+    }
 );
